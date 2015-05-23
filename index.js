bf0fbadf4f5a72262765c0589522e5e2cc55354a
Ivan Kozik <ivan@ludios.org>
2015-05-23 19:32:01 +0000

    Parse types

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 "use strong";
 "use strict";
 
-function getArgList(fn) {
+function parseTypeComment(s) {
+	const match = s.match(/\/*:(.+)\*\//, "$1");
+	let type;
+	if(match) {
+		type = match[1];
+		s = s.split('/')[0];
+	} else {
+		type = null;
+	}
+	return [s, type];
+}
+
+function getTypeStrings(fn) {
 	const source = fn.toString();
 	const argString = source.replace(/function ?\(([^\)]*)\).*/, "$1");
-	if(!argString) {
-		return [];
+	let args;
+	if(argString) {
+		args = argString
+			.split(",")
+			.map(function(source) { return source.trim(); })
+			.map(parseTypeComment);
+	} else {
+		args = [];
 	}
-	const args = argString.split(",").map(function(source) { return source.trim(); });
-	return args;
+	const ret = null;
+	return {args, ret};
 }
 
 function T(fn) {
 
 }
 
-module.exports = {getArgList};
+module.exports = {getTypeStrings};
