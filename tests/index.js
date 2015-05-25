53953a019be2459d4b66c3556fb9728d4d36656a
Ivan Kozik <ivan@ludios.org>
2015-05-25 02:01:43 +0000

    Change package name; add tests

--- tests/index.js
+++ tests/index.js
@@ -18,4 +18,21 @@ describe('T()', function() {
 			T(1, T.number, 3);
 		}, /an even number/);
 	});
+
+	it('throws error if given wrong type', function() {
+		assert.throws(function() {
+			T(new Buffer("hi"), T.string);
+		}, /First variable: Expected.*string.*got/);
+	});
+
+	it('throws error if given wrong type in second position', function() {
+		assert.throws(function() {
+			T(3, T.number, new Buffer("hi"), T.string);
+		}, /Second variable: Expected.*string.*got/);
+	});
+
+	it("doesn't throw if given correct types", function() {
+		assert.strictEqual(undefined, T(3, T.number));
+		assert.strictEqual(T(3, T.number, [new Buffer("hi")], T.list(Buffer)));
+	});
 });
